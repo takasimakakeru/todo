9a111f4926c5501fe80697c64e31d8319a45b20a
takasimakakeru <kyunpangoogle@outlook.jp>
2026-04-30 23:24:40 +0000

    a

--- my-todo-app/app/page.tsx
+++ my-todo-app/app/page.tsx
@@ -1,11 +1,2 @@
 "use client";
 import { useState } from "react";
-export default function Home() {
-    const link = document.querySelector(".link");
-    link?.addEventListener("click", () => {
-        window.location.href = "abehiroshi.la.coocan.jp"
-    })
-    return (
-        <p  className="link">リンク</p>
-    );
-}
