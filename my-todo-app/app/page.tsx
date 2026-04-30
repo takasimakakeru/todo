4678cfa8607059e7c8613743328ee58049e7591a
takasimakakeru <kyunpangoogle@outlook.jp>
2026-04-30 23:14:04 +0000

    poop

--- my-todo-app/app/page.tsx
+++ my-todo-app/app/page.tsx
@@ -1,6 +1,11 @@
 "use client";
 import { useState } from "react";
 export default function Home() {
-
-    return ();
+    const link = document.querySelector(".link");
+    link?.addEventListener("click", () => {
+        window.location.href = "abehiroshi.la.coocan.jp"
+    })
+    return (
+        <p  className="link">リンク</p>
+    );
 }
